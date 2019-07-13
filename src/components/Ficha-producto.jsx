47bee0ee7f77de5d98d4ff2dd1652b67e7edb7c4
Maik_col <m.michael1024@gmail.com>
2019-07-12 21:03:28 -0500

    travis

--- src/components/Ficha-producto.jsx
+++ src/components/Ficha-producto.jsx
@@ -3,6 +3,72 @@ import PropTypes from 'prop-types';
 
 /*import '../css/Fichaproducto.css';*/
 
+/*import React from 'react';*/
+import Conjuntoproductos from '../imagenes/ecommerce-productos/ConjuntoProductos.png';
+
+/* Importar Cerraduras electronicas */
+/* AMG100CFingerprint ATSRelojProximidad Handpunch1000 LCD701RD SchlageAD400 SchlageCO100 SchlageCO220 SchlageConnectCentury SchlageCSCamelot SchlageLE SchlageLectoresCTEMT15MT11 SchlageMultitechReaders SchlageNDE SchlageSEncode SchlageSense SchlageWP10354LE SchlageCOSeries SchlageAD400401Series SchlageCO220Series SchlageControlSeries SchlageFSeriesHandPunch SchlageMultitechCredentials*/
+import AMG100CFingerprint from '../imagenes/ecommerce-productos/Cerraduras Electronicas ControlAcceso/AMG 100C Fingerprint.png';
+/* import ATSRelojProximidad from '../imagenes/ecommerce-productos/Cerraduras Electronicas ControlAcceso/ATS Reloj Proximidad.jpg';
+import Handpunch1000 from '../imagenes/ecommerce-productos/Cerraduras Electronicas ControlAcceso/Handpunch-1000.jpg';
+import LCD701RD from '../imagenes/ecommerce-productos/Cerraduras Electronicas ControlAcceso/LCD 701RD.jpg';
+import SchlageAD400 from '../imagenes/ecommerce-productos/Cerraduras Electronicas ControlAcceso/Schlage AD400.jpg';
+import SchlageCO100 from '../imagenes/ecommerce-productos/Cerraduras Electronicas ControlAcceso/Schlage CO 100.jpg';
+import SchlageCO220 from '../imagenes/ecommerce-productos/Cerraduras Electronicas ControlAcceso/Schlage CO-220.jpg';
+import SchlageConnectCentury from '../imagenes/ecommerce-productos/Cerraduras Electronicas ControlAcceso/Schlage Connect Century.jpg';
+import SchlageCSCamelot from '../imagenes/ecommerce-productos/Cerraduras Electronicas ControlAcceso/Schlage CS Camelot.jpg';
+import SchlageLE from '../imagenes/ecommerce-productos/Cerraduras Electronicas ControlAcceso/Schlage LE.jpg';
+import SchlageLectoresCTEMT15MT11 from '../imagenes/ecommerce-productos/Cerraduras Electronicas ControlAcceso/Schlage Lectores CTE-MT15-MT11.jpg';
+import SchlageMultitechReaders from '../imagenes/ecommerce-productos/Cerraduras Electronicas ControlAcceso/Schlage Multi-tech_Readers.jpg';
+import SchlageNDE from '../imagenes/ecommerce-productos/Cerraduras Electronicas ControlAcceso/Schlage NDE.jpg';
+import SchlageSEncode from '../imagenes/ecommerce-productos/Cerraduras Electronicas ControlAcceso/Schlage S-Encode.jpg';
+import SchlageSense from '../imagenes/ecommerce-productos/Cerraduras Electronicas ControlAcceso/Schlage Sense.png';
+import SchlageWP10354LE from '../imagenes/ecommerce-productos/Cerraduras Electronicas ControlAcceso/Schlage WP-10354 - LE.jpg';
+import SchlageCOSeries from '../imagenes/ecommerce-productos/Cerraduras Electronicas ControlAcceso/Schlage-CO-Series.jpg';
+import SchlageAD400401Series from '../imagenes/ecommerce-productos/Cerraduras Electronicas ControlAcceso/Schlage-AD400-401_Series.jpg';
+import SchlageCO220Series from '../imagenes/ecommerce-productos/Cerraduras Electronicas ControlAcceso/Schlage-CO-220_Series.jpg';
+import SchlageControlSeries from '../imagenes/ecommerce-productos/Cerraduras Electronicas ControlAcceso/Schlage-Control_Series.jpg';
+import SchlageFSeriesHandPunch from '../imagenes/ecommerce-productos/Cerraduras Electronicas ControlAcceso/Schlage-F_Series_HandPunch.jpg';
+import SchlageMultitechCredentials from '../imagenes/ecommerce-productos/Cerraduras Electronicas ControlAcceso/Schlage-Multi-tech_Credentials.jpg';
+
+/* Importar Cerraduras mecanicas */
+/* BehavioralHealth */
+import BehavioralHealth from '../imagenes/ecommerce-productos/Cerraduras Mecanicas ControlAcceso/Behavioral Health.jpg';
+
+/* Importar Cierra Puertas */
+/* FalconSC60AL */
+import FalconSC60AL from '../imagenes/ecommerce-productos/Cierra Puertas/Falcon SC60AL.jpg';
+
+/* Importar Mecanismos de Suspencion */
+/*import  from '../imagenes/ecommerce-productos/Mecanismos Suspension/.png';*/
+
+/* Importar Pivotes Bisagras */
+/* BisagraContinuaInox */
+/*import BisagraContinuaInox from '../imagenes/ecommerce-productos/Pivotes Bisagras/Bisagra Continua Inox.jpg';
+
+/* Importar Redes Cableado Estructurado */
+/* Camera887x488 */
+/*import Camera887x488 from '../imagenes/ecommerce-productos/Redes Cableado Estructurado/camera-887x488.jpg';
+
+/* Importar Salidas Push Emergencia */
+/* BarraAntiPanicoBriton378 */
+import BarraAntiPanicoBriton378 from '../imagenes/ecommerce-productos/Salidas Push Emergencia/Barra Anti Panico Briton-378.jpg';
+
+/* Importar Sensores Huelgas Electronicas */
+/* SeriesElectricStrikes */
+/*import SeriesElectricStrikes from '../imagenes/ecommerce-productos/Sensores Huelgas Electronicas/45-Series-Electric-Strikes.jpg';
+
+/* Importar Acceso Suspendido Alturas */
+/* BMU540x272 */
+/*import BMU540x272 from '../imagenes/ecommerce-productos/Acceso Suspendido Alturas/BMU-540x272.jpg';
+
+/* Importar Mecanismos Suspension */
+/* import  from '../imagenes/ecommerce-productos/Mecanismos Suspension/.png';*/
+
+/**/
+
+
+
 import styled from 'styled-components';
 
 const StyledFicha = styled.div`
@@ -122,7 +188,7 @@ const Fichaproducto = ({ Producto }) => {
                 <Fichaheader>
                     <IndexSpan> {index + 1} </IndexSpan>
                     <Titulo>{titulo}</Titulo>                    
-                    <Productoimg src={imagen} alt={'imagen'} />
+                    <Productoimg src={{imagen}} alt={'imagen'} />
                 </Fichaheader>                
 
                 <FichaDetalles>
--- src/productos/Productos.js
+++ src/productos/Productos.js
@@ -1,67 +1,3 @@
-/*import React from 'react';*/
-import Conjuntoproductos from '../imagenes/ecommerce-productos/ConjuntoProductos.png';
-
-/* Importar Cerraduras electronicas */
-/* AMG100CFingerprint ATSRelojProximidad Handpunch1000 LCD701RD SchlageAD400 SchlageCO100 SchlageCO220 SchlageConnectCentury SchlageCSCamelot SchlageLE SchlageLectoresCTEMT15MT11 SchlageMultitechReaders SchlageNDE SchlageSEncode SchlageSense SchlageWP10354LE SchlageCOSeries SchlageAD400401Series SchlageCO220Series SchlageControlSeries SchlageFSeriesHandPunch SchlageMultitechCredentials*/
-import AMG100CFingerprint from '../imagenes/ecommerce-productos/Cerraduras Electronicas ControlAcceso/AMG 100C Fingerprint.png';
-/* import ATSRelojProximidad from '../imagenes/ecommerce-productos/Cerraduras Electronicas ControlAcceso/ATS Reloj Proximidad.jpg';
-import Handpunch1000 from '../imagenes/ecommerce-productos/Cerraduras Electronicas ControlAcceso/Handpunch-1000.jpg';
-import LCD701RD from '../imagenes/ecommerce-productos/Cerraduras Electronicas ControlAcceso/LCD 701RD.jpg';
-import SchlageAD400 from '../imagenes/ecommerce-productos/Cerraduras Electronicas ControlAcceso/Schlage AD400.jpg';
-import SchlageCO100 from '../imagenes/ecommerce-productos/Cerraduras Electronicas ControlAcceso/Schlage CO 100.jpg';
-import SchlageCO220 from '../imagenes/ecommerce-productos/Cerraduras Electronicas ControlAcceso/Schlage CO-220.jpg';
-import SchlageConnectCentury from '../imagenes/ecommerce-productos/Cerraduras Electronicas ControlAcceso/Schlage Connect Century.jpg';
-import SchlageCSCamelot from '../imagenes/ecommerce-productos/Cerraduras Electronicas ControlAcceso/Schlage CS Camelot.jpg';
-import SchlageLE from '../imagenes/ecommerce-productos/Cerraduras Electronicas ControlAcceso/Schlage LE.jpg';
-import SchlageLectoresCTEMT15MT11 from '../imagenes/ecommerce-productos/Cerraduras Electronicas ControlAcceso/Schlage Lectores CTE-MT15-MT11.jpg';
-import SchlageMultitechReaders from '../imagenes/ecommerce-productos/Cerraduras Electronicas ControlAcceso/Schlage Multi-tech_Readers.jpg';
-import SchlageNDE from '../imagenes/ecommerce-productos/Cerraduras Electronicas ControlAcceso/Schlage NDE.jpg';
-import SchlageSEncode from '../imagenes/ecommerce-productos/Cerraduras Electronicas ControlAcceso/Schlage S-Encode.jpg';
-import SchlageSense from '../imagenes/ecommerce-productos/Cerraduras Electronicas ControlAcceso/Schlage Sense.png';
-import SchlageWP10354LE from '../imagenes/ecommerce-productos/Cerraduras Electronicas ControlAcceso/Schlage WP-10354 - LE.jpg';
-import SchlageCOSeries from '../imagenes/ecommerce-productos/Cerraduras Electronicas ControlAcceso/Schlage-CO-Series.jpg';
-import SchlageAD400401Series from '../imagenes/ecommerce-productos/Cerraduras Electronicas ControlAcceso/Schlage-AD400-401_Series.jpg';
-import SchlageCO220Series from '../imagenes/ecommerce-productos/Cerraduras Electronicas ControlAcceso/Schlage-CO-220_Series.jpg';
-import SchlageControlSeries from '../imagenes/ecommerce-productos/Cerraduras Electronicas ControlAcceso/Schlage-Control_Series.jpg';
-import SchlageFSeriesHandPunch from '../imagenes/ecommerce-productos/Cerraduras Electronicas ControlAcceso/Schlage-F_Series_HandPunch.jpg';
-import SchlageMultitechCredentials from '../imagenes/ecommerce-productos/Cerraduras Electronicas ControlAcceso/Schlage-Multi-tech_Credentials.jpg';
-
-/* Importar Cerraduras mecanicas */
-/* BehavioralHealth */
-import BehavioralHealth from '../imagenes/ecommerce-productos/Cerraduras Mecanicas ControlAcceso/Behavioral Health.jpg';
-
-/* Importar Cierra Puertas */
-/* FalconSC60AL */
-import FalconSC60AL from '../imagenes/ecommerce-productos/Cierra Puertas/Falcon SC60AL.jpg';
-
-/* Importar Mecanismos de Suspencion */
-/*import  from '../imagenes/ecommerce-productos/Mecanismos Suspension/.png';*/
-
-/* Importar Pivotes Bisagras */
-/* BisagraContinuaInox */
-/*import BisagraContinuaInox from '../imagenes/ecommerce-productos/Pivotes Bisagras/Bisagra Continua Inox.jpg';
-
-/* Importar Redes Cableado Estructurado */
-/* Camera887x488 */
-/*import Camera887x488 from '../imagenes/ecommerce-productos/Redes Cableado Estructurado/camera-887x488.jpg';
-
-/* Importar Salidas Push Emergencia */
-/* BarraAntiPanicoBriton378 */
-import BarraAntiPanicoBriton378 from '../imagenes/ecommerce-productos/Salidas Push Emergencia/Barra Anti Panico Briton-378.jpg';
-
-/* Importar Sensores Huelgas Electronicas */
-/* SeriesElectricStrikes */
-/*import SeriesElectricStrikes from '../imagenes/ecommerce-productos/Sensores Huelgas Electronicas/45-Series-Electric-Strikes.jpg';
-
-/* Importar Acceso Suspendido Alturas */
-/* BMU540x272 */
-/*import BMU540x272 from '../imagenes/ecommerce-productos/Acceso Suspendido Alturas/BMU-540x272.jpg';
-
-/* Importar Mecanismos Suspension */
-/* import  from '../imagenes/ecommerce-productos/Mecanismos Suspension/.png';*/
-
-/**/
-
 const data = {
     "Productos": [
 
@@ -69,7 +5,7 @@ const data = {
             "_id": "1024537865klj",
             "index":0,
             "titulo": "Cerradura electronica",
-            "imagen": { Conjuntoproductos },
+            "imagen":  "Conjuntoproductos" ,
             "marca": "Schlage",
             "descripcion": "Connect Century",
             "garantia": "20 years",
@@ -80,7 +16,7 @@ const data = {
             "_id": "1024537866frt",
             "index": 1,
             "titulo": "Cerradura mecanica",
-            "imagen": {AMG100CFingerprint},
+            "imagen": "AMG100CFingerprint",
             "marca": "Schlage",
             "descripcion": "Accent cromado manija",
             "garantia": "10 years",
@@ -91,7 +27,7 @@ const data = {
             "_id": "1024536865klj",
             "index": 2,
             "titulo": "Cierra puertas",
-            "imagen": {FalconSC60AL},
+            "imagen": "FalconSC60AL",
             "marca": "Schlage",
             "descripcion": "Hidraulicos electricos",
             "garantia": "20 years",
@@ -102,7 +38,7 @@ const data = {
             "_id": "1024537860sre",
             "index": 3,
             "titulo": "Bisagras y Pivotes",
-            "imagen": { BehavioralHealth },
+            "imagen": "BehavioralHealth",
             "marca": "Ives",
             "descripcion": "A piso",
             "garantia": "15 years",
@@ -113,7 +49,7 @@ const data = {
             "_id": "1024537868hjd",
             "index": 4,
             "titulo": "Salidas Push Emergencia",
-            "imagen": { BarraAntiPanicoBriton378 },
+            "imagen": "BarraAntiPanicoBriton378",
             "marca": "Von Duprin",
             "descripcion": "Vertical rod",
             "garantia": "30 years",
@@ -124,7 +60,7 @@ const data = {
             "_id": "1782453686klj",
             "index": 5,
             "titulo": "Sensores y huelgas electronicas",
-            "imagen": { FalconSC60AL },
+            "imagen": "FalconSC60AL",
             "marca": "Schlage",
             "descripcion": "Hidraulicos electricos",
             "garantia": "10 years",
@@ -135,7 +71,7 @@ const data = {
             "_id": "1024537860hjt",
             "index": 6,
             "titulo": "Redes cableado estructurado",
-            "imagen": { BehavioralHealth },
+            "imagen": "BehavioralHealth",
             "marca": "Cuales",
             "descripcion": "Instalacion camaras",
             "garantia": "3 years",
@@ -146,7 +82,7 @@ const data = {
             "_id": "1024531268djd",
             "index": 7,
             "titulo": "Acceso suspendido alturas",
-            "imagen": { BarraAntiPanicoBriton378 },
+            "imagen": "BarraAntiPanicoBriton378",
             "marca": "SkyColoring",
             "descripcion": "Andamios y unidades permanentes",
             "garantia": "10 years",
@@ -157,7 +93,7 @@ const data = {
             "_id": "1024531268djd",
             "index": 8,
             "titulo": "Mecanismos suspencion",
-            "imagen": { BarraAntiPanicoBriton378 },
+            "imagen": "BarraAntiPanicoBriton378",
             "marca": "Black Diamond",
             "descripcion": "Mosqueton",
             "garantia": "3 years",
@@ -168,244 +104,3 @@ const data = {
 }
 
 export default data;
-/*
-    < CarrouselProducto >
-    <Titulo />
-    <a href="/components/carrousel">
-        <productoimg src={Conjuntoproductos} alt="" />
-    </a>
-    <Precio />
-</CarrouselProducto >
-
-    <CarrouselProducto>
-        <Titulo />
-        <a href="/components/carrousel">
-            <productoimg src={AMG100CFingerprint} alt="" />
-        </a>
-        <Precio />
-    </CarrouselProducto>
-
-    <CarrouselProducto>
-        <Titulo />
-        <a href="/components/carrousel">
-            <productoimg src={ATSRelojProximidad} alt="" />
-        </a>
-        <Precio />
-    </CarrouselProducto>
-
-    <CarrouselProducto>
-        <Titulo />
-        <a href="/components/carrousel">
-            <productoimg src={Handpunch1000} alt="" />
-        </a>
-        <Precio />
-    </CarrouselProducto>
-
-    <CarrouselProducto>
-        <Titulo />
-        <a href="/components/carrousel">
-            <productoimg src={LCD701RD} alt="" />
-        </a>
-        <Precio />
-    </CarrouselProducto>
-
-    <CarrouselProducto>
-        <Titulo />
-        <a href="/components/carrousel">
-            <productoimg src={SchlageAD400} alt="" />
-        </a>
-        <Precio />
-    </CarrouselProducto>
-
-    <CarrouselProducto>
-        <Titulo />
-        <a href="/components/carrousel">
-            <productoimg src={SchlageCO100} alt="" />
-        </a>
-        <Precio />
-    </CarrouselProducto>
-
-    <CarrouselProducto>
-        <Titulo />
-        <a href="/components/carrousel">
-            <productoimg src={SchlageCO220} alt="" />
-        </a>
-        <Precio />
-    </CarrouselProducto>
-
-    <CarrouselProducto>
-        <Titulo />
-        <a href="/components/carrousel">
-            <productoimg src={SchlageConnectCentury} alt="" />
-        </a>
-        <Precio />
-    </CarrouselProducto>
-
-    <CarrouselProducto>
-        <Titulo />
-        <a href="/components/carrousel">
-            <productoimg src={SchlageCSCamelot} alt="" />
-        </a>
-        <Precio />
-    </CarrouselProducto>
-
-    <CarrouselProducto>
-        <Titulo />
-        <a href="/components/carrousel">
-            <productoimg src={SchlageLE} alt="" />
-        </a>
-        <Precio />
-    </CarrouselProducto>
-
-    <CarrouselProducto>
-        <Titulo />
-        <a href="/components/carrousel">
-            <productoimg src={SchlageLectoresCTEMT15MT11} alt="" />
-        </a>
-        <Precio />
-    </CarrouselProducto>
-
-    <CarrouselProducto>
-        <Titulo />
-        <a href="/components/carrousel">
-            <productoimg src={SchlageMultitechReaders} alt="" />
-        </a>
-        <Precio />
-    </CarrouselProducto>
-
-    <CarrouselProducto>
-        <Titulo />
-        <a href="/components/carrousel">
-            <productoimg src={SchlageNDE} alt="" />
-        </a>
-        <Precio />
-    </CarrouselProducto>
-
-    <CarrouselProducto>
-        <Titulo />
-        <a href="/components/carrousel">
-            <productoimg src={SchlageSEncode} alt="" />
-        </a>
-        <Precio />
-    </CarrouselProducto>
-
-    <CarrouselProducto>
-        <Titulo />
-        <a href="/components/carrousel">
-            <productoimg src={SchlageSense} alt="" />
-        </a>
-        <Precio />
-    </CarrouselProducto>
-
-    <CarrouselProducto>
-        <Titulo />
-        <a href="/components/carrousel">
-            <productoimg src={SchlageWP10354LE} alt="" />
-        </a>
-        <Precio />
-    </CarrouselProducto>
-
-    <CarrouselProducto>
-        <Titulo />
-        <a href="/components/carrousel">
-            <productoimg src={SchlageCOSeries} alt="" />
-        </a>
-        <Precio />
-    </CarrouselProducto>
-
-    <CarrouselProducto>
-        <Titulo />
-        <a href="/components/carrousel">
-            <productoimg src={SchlageAD400401Series} alt="" />
-        </a>
-        <Precio />
-    </CarrouselProducto>
-
-    <CarrouselProducto>
-        <Titulo />
-        <a href="/components/carrousel">
-            <productoimg src={SchlageCO220Series} alt="" />
-        </a>
-        <Precio />
-    </CarrouselProducto>
-
-    <CarrouselProducto>
-        <Titulo />
-        <a href="/components/carrousel">
-            <productoimg src={SchlageControlSeries} alt="" />
-        </a>
-        <Precio />
-    </CarrouselProducto>
-
-    <CarrouselProducto>
-        <Titulo />
-        <a href="/components/carrousel">
-            <productoimg src={SchlageFSeriesHandPunch} alt="" />
-        </a>
-        <Precio />
-    </CarrouselProducto>
-
-    <CarrouselProducto>
-        <Titulo />
-        <a href="/components/carrousel">
-            <productoimg src={SchlageMultitechCredentials} alt="" />
-        </a>
-        <Precio />
-    </CarrouselProducto>
-
-    <CarrouselProducto>
-        <Titulo />
-        <a href="/components/carrousel">
-            <productoimg src={BehavioralHealth} alt="" />
-        </a>
-        <Precio />
-    </CarrouselProducto>
-
-    <CarrouselProducto>
-        <Titulo />
-        <a href="/components/carrousel">
-            <productoimg src={FalconSC60AL} alt="" />
-        </a>
-        <Precio />
-    </CarrouselProducto>
-
-    <CarrouselProducto>
-        <Titulo />
-        <a href="/components/carrousel">
-            <productoimg src={BisagraContinuaInox} alt="" />
-        </a>
-        <Precio />
-    </CarrouselProducto>
-
-    <CarrouselProducto>
-        <Titulo />
-        <a href="/components/carrousel">
-            <productoimg src={Camera887x488} alt="" />
-        </a>
-        <Precio />
-    </CarrouselProducto>
-
-    <CarrouselProducto>
-        <Titulo />
-        <a href="/components/carrousel">
-            <productoimg src={BarraAntiPanicoBriton378} alt="" />
-        </a>
-        <Precio />
-    </CarrouselProducto>
-
-    <CarrouselProducto>
-        <Titulo />
-        <a href="/components/carrousel">
-            <productoimg src={SeriesElectricStrikes} alt="" />
-        </a>
-        <Precio />
-    </CarrouselProducto>
-
-    <CarrouselProducto>
-        <Titulo />
-        <a href="/components/carrousel">
-            <productoimg src={BMU540x272} alt="" />
-        </a>
-        <Precio />
-    </CarrouselProducto>
-    */
