da06d1539b259143816672e1e66ee7c01949926d
Maik_col <m.michael1024@gmail.com>
2019-07-12 21:11:21 -0500

    Updates

---
+++ precache-manifest.0c9c64918bd9051ca5f69a1519287926.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "975029be8d53e17357fec87f7a3020cd",
+    "url": "/Listtech/index.html"
+  },
+  {
+    "revision": "f8f16ee22cc32efdfd76",
+    "url": "/Listtech/static/css/main.cd5361ff.chunk.css"
+  },
+  {
+    "revision": "61903f754801e383ff7b",
+    "url": "/Listtech/static/js/2.96106ee6.chunk.js"
+  },
+  {
+    "revision": "f8f16ee22cc32efdfd76",
+    "url": "/Listtech/static/js/main.91c76078.chunk.js"
+  },
+  {
+    "revision": "275bfba1d7727ddad115",
+    "url": "/Listtech/static/js/runtime~main.0fb07db0.js"
+  },
+  {
+    "revision": "b6291e9d724a24586b118a44623a75e0",
+    "url": "/Listtech/static/media/AMG 100C Fingerprint.b6291e9d.png"
+  },
+  {
+    "revision": "b493eebb322815592212e5195ff10c8b",
+    "url": "/Listtech/static/media/Barra Anti Panico Briton-378.b493eebb.jpg"
+  },
+  {
+    "revision": "9292dbe388a65af48aca95def2cfd95a",
+    "url": "/Listtech/static/media/Behavioral Health.9292dbe3.jpg"
+  },
+  {
+    "revision": "fde1672b2a207ddd5a67d5ed0165baa3",
+    "url": "/Listtech/static/media/ConjuntoProductos.fde1672b.png"
+  },
+  {
+    "revision": "1c4cfcafe4424ac8459aab1a932cdd7a",
+    "url": "/Listtech/static/media/Falcon SC60AL.1c4cfcaf.jpg"
+  },
+  {
+    "revision": "2680bdd583eca36478eea051f40dd080",
+    "url": "/Listtech/static/media/Listtech-Logo.2680bdd5.png"
+  },
+  {
+    "revision": "1ad4e7a10ad76c0edfca374e4cb3340b",
+    "url": "/Listtech/static/media/americanexpress.1ad4e7a1.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Listtech/precache-manifest.0c9c64918bd9051ca5f69a1519287926.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Listtech/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
